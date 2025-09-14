a7f46e1b9d213c423e8efb5c3091ac320e298516
vaibhavi telrandhe <vaibhavitelrandhe28@gmail.com>
2025-09-14 13:40:33 +0530

    change in login form

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -94,7 +94,7 @@ const Login = () => {
   return (
     <div>
       <Navbar />
-      <div className="flex items-center justify-center max-w-7xl mx-auto mb-5">
+      <div className="flex items-center justify-center max-w-7xl mx-auto mb-4">
         <form
           onSubmit={submitHandler}
           className="w-1/2 border border-gray-200 rounded-md p-4 my-10"
